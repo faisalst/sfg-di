c320aefafd71c4dfa407c80804b5a4ebb06214ab
unknown <faisalmad68@gmail.com>
2021-08-03 10:50:19 -0700

    default profile

--- src/main/java/guru/sprintframework/sfgd/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/sprintframework/sfgd/services/I18nEnglishGreetingService.java
@@ -3,7 +3,7 @@ package guru.sprintframework.sfgd.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("En")
+@Profile({"En", "default"})
 @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService{
     @Override
